79a1f24ab0f05b9e1dc02d61776f289cb79a360c
monkeydri <monkeydri@github.com>
2017-07-01 21:05:29 +0200

    take proxy list as command line arguments

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-proxy-checker
-=============
-A simple python script to check if a proxy is working.
+**proxy-checke** python script to check if a proxy is working.
 
-Simply put proxy:port in array. If you want to check if internet is working or not, leave the array empty.
+# Usage
+
+`python proxy_check.py '172.30.1.1:8080' '172.30.3.3:8080'`
+
+If you want to check if internet is working or not, leave the array empty.
--- proxy_check.py
+++ proxy_check.py
@@ -1,29 +1,30 @@
-import urllib2, socket
+import urllib2, socket, sys
 
 socket.setdefaulttimeout(180)
 
-# read the list of proxy IPs in proxyList
-proxyList = ['172.30.1.1:8080', '172.30.3.3:8080'] # there are two sample proxy ip
+# 0. Read the list of proxy provided as arguments
+# remove the name of the script in the arguments array
+sys.argv.pop(0)
+proxyList = sys.argv
 
-def is_bad_proxy(pip):    
-    try:        
-        proxy_handler = urllib2.ProxyHandler({'http': pip})        
-        opener = urllib2.build_opener(proxy_handler)
-        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        urllib2.install_opener(opener)        
-        req=urllib2.Request('http://www.google.com')  # change the url address here
-        sock=urllib2.urlopen(req)
-    except urllib2.HTTPError, e:        
-        print 'Error code: ', e.code
-        return e.code
-    except Exception, detail:
-
-        print "ERROR:", detail
-        return 1
-    return 0
+def is_bad_proxy(pip):
+	try:
+		proxy_handler = urllib2.ProxyHandler({'http': pip})
+		opener = urllib2.build_opener(proxy_handler)
+		opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+		urllib2.install_opener(opener)
+		req=urllib2.Request('http://www.google.com')  # change the url address here
+		sock=urllib2.urlopen(req)
+	except urllib2.HTTPError, e:
+		print 'Error code: ', e.code
+		return e.code
+	except Exception, detail:
+		print "ERROR:", detail
+		return 1
+		return 0
 
 for item in proxyList:
-    if is_bad_proxy(item):
-        print "Bad Proxy", item
-    else:
-        print item, "is working"
+	if is_bad_proxy(item):
+		print "Bad Proxy", item
+	else:
+		print item, "is working"
